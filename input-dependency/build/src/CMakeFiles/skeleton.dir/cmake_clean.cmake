1fe792258b8831858872e5ad8f25406a8042e30b
Zach Stone <z.stone91@gmail.com>
2017-06-15 20:56:14 +0000

    Check in provided input-dependency files

---
+++ input-dependency/build/src/CMakeFiles/skeleton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skeleton.dir/InputDependencySkeleton.cpp.o"
+  "../lib/libskeleton.pdb"
+  "../lib/libskeleton.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skeleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
