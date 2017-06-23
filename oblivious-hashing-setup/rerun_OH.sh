006b9be1b5672de01771326236f6243303fd78dc
VitaNuova <vitanuova0512@gmail.com>
2017-06-23 14:02:14 +0000

    script edited

--- oblivious-hashing-setup/rerun_OH.sh
+++ oblivious-hashing-setup/rerun_OH.sh
@@ -2,22 +2,17 @@
 # sudo rm bubble_out.bc
 # sudo rm bubble_out.o
 # sudo rm a.out
-
-cd build
-make
-cd ..
-
-# InputFilename="bubble_sort.cpp"
+rm -rf *.bc *o *.out
 
 # Compile hash snippets
-cc -c src/OHHashFunctions.cpp
+cc -c /home/sip/sip-oblivious-hashing/oblivious-hashing-setup/src/OHHashFunctions.c 
 
-clang-3.9 -emit-llvm ../bubble_sort_print.cpp -c -o bubblebc.bc
-opt-3.9 -load /usr/local/lib/libInputDependency.so -load ./build/lib/libOHSetup.so bubblebc.bc -numHashVars 5 -oh-setup-pass -o bubble_out.bc
+clang-3.9 -emit-llvm /home/sip/sip-oblivious-hashing/bubble_sort_print.c -c -o bubblebc.bc
+
+opt-3.9  -load /home/sip/sip-oblivious-hashing/oblivious-hashing-setup/build/lib/libOHSetup.so bubblebc.bc -numHashVars 5 -oh-setup-pass -o bubble_out.bc
 # Another pass here if needed?
 llc-3.9 -filetype=obj ./bubble_out.bc
 
-# cc bubble_out.o
-cc -rdynamic bubble_out.o OHHashFunctions.o
+cc bubble_out.o OHHashFunctions.o
 
 ./a.out 5
